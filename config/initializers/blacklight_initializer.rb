9b0c9002d144537660f46eea798f38557b117c5a
Shubhanshu Gupta <shubhanshu2007@gmail.com>
2015-01-18 01:03:28 +0530

    Setting up hydra head

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+Blacklight.secret_key = 'ca382f3ee54a76c9241d8f5c2468436a5b158c34df6555854dea9621ae6c91a9e07b8942753af30f10fcda0049029a511dafb0b677bb5fab8181869e0e03f7fe'
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hydra::Application.config.secret_token = 'e2d135e3bd902299ae9ec861ff4494985f6dae4615c5a52724f19a00c2bb68b0d4d1f89c38c526b654ad928d69b4f840160ef5e44149d8f18969e34e1993c963'
